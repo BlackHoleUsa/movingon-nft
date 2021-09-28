8fe2d825dbb465ca2577b0063467ab7c2ad00c21
ArbazKhan5117 <68884805+ArbazKhan5117@users.noreply.github.com>
2021-09-28 11:09:21 +0500

    books section design is updated

--- src/screens/Main/Books/Books.js
+++ src/screens/Main/Books/Books.js
@@ -26,8 +26,11 @@ const Books = (props) => {
             <h2 className="books-h6">Our Books</h2>
             <Row className="m-0 p-0">
                 <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                    
+                    <img alt="" width="120%" src={book2} />
+                   {/* <div className="book-title">
 
-                    <img alt="" width="100%" src={book2} />
+                    </div>*/}
                     <div className="book-buy-links">
                     <spam className="book-name">Butterflies in Production</spam>
                     <div className="line"></div>
@@ -36,9 +39,9 @@ const Books = (props) => {
                     </div>
                 </Col>
 
-                <Col xs={12} sm={12} md={6} lg={6} xl={6} className="px-4 mt-3">
-                    <img alt="" width="95%" src={book1}/>
-                    <div className="book-buy-links">
+                <Col xs={12} sm={12} md={6} lg={6} xl={6} className="px-4 mt-3" >
+                    <img alt="" width="120%" src={book1} style={{marginLeft: '-20%'}}/>
+                    <div className="book-buy-links" style={{marginLeft: '0%', marginTop: '-6%'}}>
                     <spam className="book-name-rightSection">Moving On</spam>
                     <div className="line"></div>
                     <spam className="book-name amazon-link" onClick={()=>goToLink('movingOn')}>Buy book on Amazon <img alt="" width="10px" src={arrow} /></spam>
