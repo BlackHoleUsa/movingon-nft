118a1543e6ed0ef7a0c6fe33ee0962434948d78d
aliraza944 <alisabri022@gmail.com>
2021-11-08 12:39:45 +0500

    Fix: Added the logo to the navbar

--- src/Assets/Images.js
+++ src/Assets/Images.js
@@ -1,44 +1,45 @@
 export const Images = {
-    author: require('./images/author.png').default,
-    city: require('./images/city.png').default,
-    headerBook: require('./images/header-book.png').default,
+  author: require("./images/author.png").default,
+  city: require("./images/city.png").default,
+  headerBook: require("./images/header-book.png").default,
 
-    chooseBookIcons: [
-        require('./images/transfer.png').default,
-        require('./images/trust.png').default,
-        require('./images/coins.png').default
-    ],
+  chooseBookIcons: [
+    require("./images/transfer.png").default,
+    require("./images/trust.png").default,
+    require("./images/coins.png").default,
+  ],
 
-    owners: [
-        require('./images/owner1.png').default,
-        require('./images/owner2.png').default,
-        require('./images/owner3.png').default
-    ],
+  owners: [
+    require("./images/owner1.png").default,
+    require("./images/owner2.png").default,
+    require("./images/owner3.png").default,
+  ],
 
-    pages: [
-        require('./images/page1.png').default,
-        require('./images/page2.png').default,
-        require('./images/page3.png').default
-    ],
+  pages: [
+    require("./images/page1.png").default,
+    require("./images/page2.png").default,
+    require("./images/page3.png").default,
+  ],
 
-    clients: [
-        require('./images/client1.png').default,
-        require('./images/client2.png').default,
-        require('./images/client3.png').default
-    ],
+  clients: [
+    require("./images/client1.png").default,
+    require("./images/client2.png").default,
+    require("./images/client3.png").default,
+  ],
 
-    pat: require('./images/pat.png').default,
+  pat: require("./images/pat.png").default,
 
-    features: [
-        require('./images/content-copy.png').default,
-        require('./images/diamond.png').default,
-        require('./images/diagram.png').default,
-        require('./images/open-people.png').default
-    ],
+  features: [
+    require("./images/content-copy.png").default,
+    require("./images/diamond.png").default,
+    require("./images/diagram.png").default,
+    require("./images/open-people.png").default,
+  ],
 
-    aboutImage: require('./images/about-img.png').default,
-    bgDashboard: require('./images/bg-dashboard.png').default,
-    bgConnect: require('./images/bg-connect.png').default,
-    movingDashboard: require('./images/moving-dashboard.png').default,
-    butterflyDashboard: require('./images/butterfly-dashboard.png').default
-}
+  aboutImage: require("./images/about-img.png").default,
+  bgDashboard: require("./images/bg-dashboard.png").default,
+  bgConnect: require("./images/bg-connect.png").default,
+  movingDashboard: require("./images/moving-dashboard.png").default,
+  butterflyDashboard: require("./images/butterfly-dashboard.png").default,
+  logo: require("./images/logo.png").default,
+};
--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,150 +1,163 @@
-import React, { useEffect, useState } from 'react';
-import './Topbar.css';
-import { NavLink,Link } from 'react-router-dom';
-import { Images } from 'Assets/Images';
-import { Routes } from 'Routes/Routes';
-import { topBarContent } from 'Assets/Data';
-import { BiChevronDown } from 'react-icons/bi';
-import { Nav, Navbar} from 'react-bootstrap';
-import AmazonModal from 'components/Modal/AmazonModel';
-import { useDispatch, useSelector } from 'react-redux';
-import { connectMetaMask, checkAlreadyConnectedMetaMask } from 'redux/thunk/thunk';
+import React, { useEffect, useState } from "react";
+import "./Topbar.css";
+import { NavLink, Link } from "react-router-dom";
+import { Images } from "Assets/Images";
+import { Routes } from "Routes/Routes";
+import { topBarContent } from "Assets/Data";
+import { BiChevronDown } from "react-icons/bi";
+import { Nav, Navbar } from "react-bootstrap";
+import AmazonModal from "components/Modal/AmazonModel";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  connectMetaMask,
+  checkAlreadyConnectedMetaMask,
+} from "redux/thunk/thunk";
 
 const Topbar = (props) => {
-    
-    // redux setups
-    const dispatch = useDispatch();
-    const state = useSelector(state => state);
+  // redux setups
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
-    const [ scroll, setScroll ] = useState(false);
+  const [scroll, setScroll] = useState(false);
 
-    const [ showMenu, setShowMenu ] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    const clientAddress = '0x0F7217FA0Ed45b019481F9E74b9c329245Afe84B';
+  const clientAddress = "0x0F7217FA0Ed45b019481F9E74b9c329245Afe84B";
 
-    const { currentState, clickLink } = props;
+  const { currentState, clickLink } = props;
 
-    const connectToMetaMask = () => {
-      dispatch(connectMetaMask());
+  const connectToMetaMask = () => {
+    dispatch(connectMetaMask());
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+
+    const interval = setInterval(() => {
+      dispatch(checkAlreadyConnectedMetaMask(state?.connection));
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', listenToScroll);
-        
-        const interval = setInterval(() => {
-            dispatch(checkAlreadyConnectedMetaMask(state?.connection));
-        }, 3000);
-      
-        return () => {
-            clearInterval(interval);
-        };
+  const listenToScroll = () => {
+    let i = 0;
 
-    }, []);
-
-    const listenToScroll = () => {
-        let i = 0;
-
-        if(window.pageYOffset > 118){
-            i = i+1;
-            if(i === 1){
-                setScroll(true);
-            }
-        } else{
-            setScroll(false);
-        }
-    };
-
-    const goToLink = (index, link) => {
-        if(index === 8){
-            // replace link with client specific link
-           // window.open('https://www.amazon.com/dp/B08MHMP2M7/ref=cm_sw_r_cp_api_glt_fabc_6GF5BAGVHZSZ9YYR0ZB9', '_blank');
-            
-        } else{
-            clickLink({ value: link.value.toLowerCase(), sectionId: link.sectionId });
-        }
+    if (window.pageYOffset > 118) {
+      i = i + 1;
+      if (i === 1) {
+        setScroll(true);
+      }
+    } else {
+      setScroll(false);
     }
-    
-    return(
+  };
 
-        <Navbar collapseOnSelect expand="lg" className={`
+  const goToLink = (index, link) => {
+    if (index === 8) {
+      // replace link with client specific link
+      // window.open('https://www.amazon.com/dp/B08MHMP2M7/ref=cm_sw_r_cp_api_glt_fabc_6GF5BAGVHZSZ9YYR0ZB9', '_blank');
+    } else {
+      clickLink({ value: link.value.toLowerCase(), sectionId: link.sectionId });
+    }
+  };
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className={`
             navbar navbar-expand-lg navbar-light fixed-top padding-topbar
-            ${ (scroll || showMenu) ? 'bg-lightPrimary' : '' }
-        `}>
-        
-            <NavLink to={`/${Routes.main}`}>
-                {/*<span className="text-uppercase font-28px text-black mr-4" style={{fontStyle: 'italic', fontFamily: 'Oswald'}}>Rare Jewels</span>*/}
-            </NavLink>
-        
-            <Navbar.Toggle className={` ${scroll ? 'bg-lightPurple' : 'bg-whiteSmoke'} border-0`} 
-            onClick={() => setShowMenu(!showMenu)} />
+            ${scroll || showMenu ? "bg-lightPrimary" : ""}
+        `}
+    >
+      <NavLink to={`/${Routes.main}`}>
+        <span
+          className="text-uppercase font-28px text-black mr-4"
+          style={{ fontStyle: "italic", fontFamily: "Oswald" }}
+        >
+          <img src={Images.logo} alt="" />
+        </span>
+      </NavLink>
 
-            <Navbar.Collapse>
-                <Nav className="navbar-nav ml-auto app-flex-column justify-content-center align-items-center">
+      <Navbar.Toggle
+        className={` ${scroll ? "bg-lightPurple" : "bg-whiteSmoke"} border-0`}
+        onClick={() => setShowMenu(!showMenu)}
+      />
 
-                    {
-                        topBarContent.map((link, i) => (
-                            <Nav.Item 
-                                onClick={() => goToLink(i, link)}
-                                style={{fontWeight: 'bold'}}
-                                className={` nav-item p-0 nav-links heading-font
-                                ${ (currentState.toLowerCase() === link.value.toLowerCase()) ? 
-                                    'gradient-apply active-link' : 'inactive-link' } 
-                                `} 
-                                key={i}
-                            >
-                                
-                                <span className={ (scroll || showMenu) ? 'text-black' : 'text-black' }>
-                                {link.sectionId === 'buyNft-section' ?
-                                    <Link to={`/${Routes.connect}`} style={{color: 'black'}}>
-                                    
-            
-                                 {/* check to go on Dashboard */}
-                                </Link>: link.sectionId === 'dashboard-section' && clientAddress === state?.address[0] ? <Link to={`/${Routes.dashboard}`}><spam style={{color: 'black'}}>Dashboard</spam></Link> : link.value
-                                    }
-                                </span>
+      <Navbar.Collapse>
+        <Nav className="navbar-nav ml-auto app-flex-column justify-content-center align-items-center">
+          {topBarContent.map((link, i) => (
+            <Nav.Item
+              onClick={() => goToLink(i, link)}
+              style={{ fontWeight: "bold" }}
+              className={` nav-item p-0 nav-links heading-font
+                                ${
+                                  currentState.toLowerCase() ===
+                                  link.value.toLowerCase()
+                                    ? "gradient-apply active-link"
+                                    : "inactive-link"
+                                } 
+                                `}
+              key={i}
+            >
+              <span
+                className={scroll || showMenu ? "text-black" : "text-black"}
+              >
+                {link.sectionId === "buyNft-section" ? (
+                  <Link to={`/${Routes.connect}`} style={{ color: "black" }}>
+                    {/* check to go on Dashboard */}
+                  </Link>
+                ) : link.sectionId === "dashboard-section" &&
+                  clientAddress === state?.address[0] ? (
+                  <Link to={`/${Routes.dashboard}`}>
+                    <spam style={{ color: "black" }}>Dashboard</spam>
+                  </Link>
+                ) : (
+                  link.value
+                )}
+              </span>
+            </Nav.Item>
+          ))}
 
-                            </Nav.Item>
-                        ))
-                    }
+          <button
+            className="connect-wallet pb-0 gradient-apply border-0 connect-meta-mask"
+            onClick={connectToMetaMask}
+            disabled={state?.connection}
+          >
+            {!state?.connection ? (
+              <>Connect Wallet</>
+            ) : (
+              <div className="app-flex-row align-items-center text-white justify-content-center">
+                <div
+                  className="app-flex-column heading-font bg-blue align-items-center justify-content-center p-2 font-14px rounded-circle"
+                  style={{ width: "32px", height: "32px" }}
+                >
+                  U
+                </div>
 
-                    <button className="connect-wallet pb-0 gradient-apply border-0 connect-meta-mask" 
-                      onClick={connectToMetaMask}
-                      disabled={state?.connection}
-                    >
-                      { 
-                        !state?.connection ? <>Connect Wallet</> 
-                        : <div className="app-flex-row align-items-center text-white justify-content-center">
-                            
-                            <div className="app-flex-column heading-font bg-blue align-items-center justify-content-center p-2 font-14px rounded-circle" 
-                            style={{ width: '32px', height: '32px' }}>
-                              U
-                            </div>
+                <span className="font-14px ml-3 heading-font">
+                  {" "}
+                  {state?.address[0]?.substr(0, 10)}{" "}
+                </span>
+              </div>
+            )}
+          </button>
 
-                            <span className="font-14px ml-3 heading-font"> { state?.address[0]?.substr(0, 10) } </span>
-
-                        </div>
-                      }
-                    
-                    </button>
-
-                    { 
-                     /* state?.connection &&
+          {/* state?.connection &&
                       <NavLink exact to={`/${Routes.connect}`} className={`connect-wallet text-white gradient-apply pb-0 w-auto pl-4 pr-3 margin-buy-btn heading-font
                           ${ (scroll || showMenu) ? 'inactive-link-dark' : 'inactive-link' } `} 
                         >
                           
                         BUY <BiChevronDown className="ml-1 font-18px" />
                       
-                      </NavLink>*/
-                    }
+                      </NavLink>*/}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-                </Nav>
-            </Navbar.Collapse>
-
-        </Navbar>
-
-    );
-
-}
- 
 export default Topbar;
